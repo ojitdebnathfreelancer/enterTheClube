c023f455bc6cf13712f62139d8234980d8dca173
shuvornoy <shuvornoy@gmaol.com>
2023-04-03 16:25:51 +0600

    width update

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
       <div className="max-container px-5">
         <div className="flex justify-between items-center mt-[16px]">
           <div className="xl:h-[70px] lg:h-[60px] md:h-[50px] h-[40px]">
-            <img className="h-full" src={logo} alt="" />
+            <img className="h-full" src={logo} alt="logo" />
           </div>
           <div className="xl:text-[22px] lg:text-[18px] md:text-[16px] text-[14px] flex gap-3">
             <BsTwitter />
@@ -16,13 +16,15 @@ const Landing = () => {
             <BsInstagram />
           </div>
         </div>
-        <div className="flex justify-center items-center xl:h-[500px] lg:h-[400px] md:h-[300px] h-[200px]">
-          <img className="h-full" src={landingImg} alt="" />
-        </div>
-        <div className="text-center">
-          <button className="border-2 border-gray-900 rounded px-6 py-2">
-            Enter The BroTheRhood
-          </button>
+        <div>
+          <div className="flex justify-center items-center xl:h-[500px] lg:h-[420px] md:h-[300px] h-[250px]">
+            <img className="h-full" src={landingImg} alt="landingImg" />
+          </div>
+          <div className='text-center'>
+            <button className="border-2 border-gray-900 rounded xl:text-[20px] lg:text-[18px] md:text-[16px] text-[14px] xl:px-5 lg:px-4 md:px-2 px-2 py-2 fontTrash">
+              Enter The BroTheRhood
+            </button>
+          </div>
         </div>
       </div>
     );
