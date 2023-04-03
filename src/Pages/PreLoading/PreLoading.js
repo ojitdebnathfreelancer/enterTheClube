3514a4a1b57d59d6594d41b863995cc2706f00a2
unknown <ojitdebnathfreelancer.com>
2023-04-03 19:53:13 +0600

    loader added

--- src/Pages/PreLoading/PreLoading.js
+++ src/Pages/PreLoading/PreLoading.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from '../../assets/preLoadinLogo.png';
 
 const PreLoading = () => {
     return (
-        <div className=''>
-            <h1>Loading page</h1>
+        <div className='pre-loading-bg min-h-screen flex justify-center items-center'>
+            <div className=''>
+                <img className='lg:h-[220px] md:h-[200px] h-[150px]' src={logo} alt='logo'></img>
+                <div className='lg:w-[200px] md:w-[185px] w-[150px] lg:h-[20px] md:h-[20px] h-[15px] mx-auto loader'></div>
+            </div>
         </div>
     );
 };
