d1f1d7a01db3acf8de4b9aced408c80bbb2eaeaa
unknown <ojitdebnathfreelancer.com>
2023-04-03 14:03:25 +0600

    componets setup

---
+++ src/Pages/Home/ApplyFor/ApplyFor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApplyFor = () => {
+    return (
+        <div>
+            <h1>apply for section</h1>
+        </div>
+    );
+};
+
+export default ApplyFor;
---
+++ src/Pages/Home/TheHodlerGame/TheHodlerGame.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TheHodlerGame = () => {
+    return (
+        <div>
+            <h1>The hodler game section</h1>
+        </div>
+    );
+};
+
+export default TheHodlerGame;
