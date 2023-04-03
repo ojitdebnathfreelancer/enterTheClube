2d314e587297248a0a01cfaa37f1c41a341ddb51
shuvornoy <shuvornoy@gmaol.com>
2023-04-03 16:02:41 +0600

    update

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -5,18 +5,18 @@ import { BsTwitter, BsDiscord, BsInstagram } from "react-icons/bs";
 
 const Landing = () => {
     return (
-      <div className="max-container">
+      <div className="max-container px-5">
         <div className="flex justify-between items-center mt-[16px]">
           <div className="xl:h-[70px] lg:h-[60px] md:h-[50px] h-[40px]">
             <img className="h-full" src={logo} alt="" />
           </div>
-          <div className="text-[22px] flex gap-3">
+          <div className="xl:text-[22px] lg:text-[18px] md:text-[16px] text-[14px] flex gap-3">
             <BsTwitter />
             <BsDiscord />
             <BsInstagram />
           </div>
         </div>
-        <div className="flex justify-center items-center h-[500px]">
+        <div className="flex justify-center items-center xl:h-[500px] lg:h-[400px] md:h-[300px] h-[200px]">
           <img className="h-full" src={landingImg} alt="" />
         </div>
         <div className="text-center">
