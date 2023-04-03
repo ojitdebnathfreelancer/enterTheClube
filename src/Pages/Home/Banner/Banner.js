892a6b774b776e5de3b60cf7cd9b12b7545e109d
Ripassorker <ripassorker02@glail.com>
2023-04-03 20:45:30 +0600

    added banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/logo.png";
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>Banner section</h1>
-        </div>
-    );
+   return (
+      <div className="px-4">
+         <div className="flex justify-between items-center py-2">
+            <img className="h-16" src={logo} alt="" />
+            <p className="text-white">manu</p>
+         </div>
+         <div className="max-container text-white text-center lg:leading-[170px] leading-[120px]">
+            <h1 className="lg:text-[180px] md:text-[130px] text-[110px]">
+               CRYPTO
+            </h1>
+            <h1 className="lg:text-[180px] md:text-[130px] text-[110px]">
+               BROS
+            </h1>
+            <p className="fontTrash text-[#5accf5] md:text-2xl text-xl md:mt-6">
+               Welcome To The BrotherHood
+            </p>
+         </div>
+      </div>
+   );
 };
 
 export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import OurCollection from "./OurCollection/OurCollection";
 import TheHodlerGame from "./TheHodlerGame/TheHodlerGame";
 import ApplyFor from "./ApplyFor/ApplyFor";
 import Noanimal from "./NoAnimal/Noanimal";
+import Banner from "./Banner/Banner";
 
 const Home = () => {
    return (
@@ -10,6 +11,7 @@ const Home = () => {
          className=""
          style={{ background: "#000000 0% 0% no-repeat padding-box" }}
       >
+         <Banner />
          <OurCollection />
          <TheHodlerGame />
          <Noanimal />
