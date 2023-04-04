a758065075b0c4efacbeaad279fd7e6e4993e413
Ripassorker <ripassorker02@glail.com>
2023-04-04 13:20:46 +0600

    done my work

--- src/Pages/Home/NoAnimal/Noanimal.js
+++ src/Pages/Home/NoAnimal/Noanimal.js
@@ -7,17 +7,17 @@ const Noanimal = () => {
       <div className="lg:mt-[200px] md:mt-[150px] mt-[50px] px-4">
          <div className="max-container ">
             <div className="grid md:grid-cols-4 gap-6 lg:w-[950px] mx-auto mt-3">
-               <div className="col-span-1">
+               <div className="col-span-1 flex justify-center items-center">
                   <img className="rounded-md w-full" src={img} alt="" />
                </div>
                <div className="md:col-span-3 lg:ml-28">
                   <Title title={"No animal pfp bullsh*t."} />
-                  <p className="text-gray-200 fontSfReg mb-3">
+                  <p className="text-gray-200 fontSfReg md:leading-[30px] md:text-[20px]  mb-3">
                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
                      Quia magnam vel eos fuga voluptate esse minus consequatur
                      sunt accusamus quae.
                   </p>
-                  <p className="text-gray-200 fontSfReg">
+                  <p className="text-gray-200 fontSfReg md:leading-[30px] md:text-[20px]  ">
                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
                      Quia magnam vel eos fuga voluptate esse minus consequatur
                      sunt accusamus quae. fuga voluptate esse minus consequatur
--- src/Pages/Home/OurCollection/OurCollection.js
+++ src/Pages/Home/OurCollection/OurCollection.js
@@ -12,7 +12,7 @@ const OurCollection = () => {
             <Title title={"our collections"} classes={"text-center"} />
             <div className="grid md:grid-cols-2 gap-6 lg:w-[950px] mx-auto mt-3">
                <div
-                  className=" rounded-md max- h-[260px]"
+                  className=" rounded-md max- lg:h-[300px] md:h-[330px] h-[300px]"
                   style={{
                      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${img1})`,
                      backgroundRepeat: "no-repeat",
@@ -21,25 +21,25 @@ const OurCollection = () => {
                      width: "100%",
                   }}
                >
-                  <div className="p-5 lg:mt-4">
-                     <h3 className="text-gray-200 text-xl font-semibold ">
+                  <div className="p-5 ">
+                     <h3 className="text-gray-200 md:text-[24px] text-[21px] font-semibold ">
                         MAY 2023
                      </h3>
-                     <h3 className="text-gray-50 text-2xl font-bold mt-1 mb-2">
+                     <h3 className="text-gray-50 md:text-[34px] text-[28px] fontSfBold mt-1 mb-2">
                         The CryptoBros
                      </h3>
-                     <p className="text-gray-200 fontSfReg md:mr-5">
+                     <p className="text-gray-200 fontSfReg md:mr-5 md:text-[20px] ">
                         The CryptoBros our primiere collection od 8,888 PFPs
                         that serve as a membership card to utility and perks
                         available exclusively to the Brotherhood.
                      </p>
-                     <button className="bg-white text-gray-950 rounded-lg px-3 py-1 mt-3">
+                     <button className="bg-white text-gray-950 fontSfMed rounded-lg px-3 py-1 mt-3">
                         Learn More
                      </button>
                   </div>
                </div>
                <div
-                  className=" rounded-md max- h-[260px]"
+                  className=" rounded-md max- lg:h-[300px] md:h-[330px] h-[300px]"
                   style={{
                      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${img2})`,
                      backgroundRepeat: "no-repeat",
@@ -48,25 +48,25 @@ const OurCollection = () => {
                      width: "100%",
                   }}
                >
-                  <div className="p-5 lg:mt-4">
-                     <h3 className="text-gray-200 text-xl font-semibold ">
-                        MAY 2023
+                  <div className="p-5 ">
+                     <h3 className="text-gray-200 md:text-[24px] text-[21px] font-semibold ">
+                        JUNE 2023
                      </h3>
-                     <h3 className="text-gray-50 text-2xl font-bold mt-1 mb-2">
-                        The CryptoBros
+                     <h3 className="text-gray-50 md:text-[34px] text-[28px] fontSfBold mt-1 mb-2">
+                        Season I: Brordinals
                      </h3>
-                     <p className="text-gray-200 fontSfReg md:mr-5">
-                        The CryptoBros our primiere collection od 8,888 PFPs
-                        that serve as a membership card to utility and perks
-                        available exclusively to the Brotherhood.
+                     <p className="text-gray-200 fontSfReg md:mr-5 md:text-[20px]">
+                        A first of its kind limited edition collection
+                        consisting of 10 winnable and 10 honorary one-of-one
+                        CryptoBro ordinals, inscribed on the Bitcoin blockchain.
                      </p>
-                     <button className="bg-white text-gray-950 rounded-lg px-3 py-1 mt-3">
+                     <button className="bg-white text-gray-950 fontSfMed rounded-lg px-3 py-1 mt-3">
                         Learn More
                      </button>
                   </div>
                </div>
                <div
-                  className=" rounded-md max- h-[260px]"
+                  className=" rounded-md max- lg:h-[300px] md:h-[330px] h-[300px]"
                   style={{
                      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${img3})`,
                      backgroundRepeat: "no-repeat",
@@ -75,25 +75,22 @@ const OurCollection = () => {
                      width: "100%",
                   }}
                >
-                  <div className="p-5 lg:mt-4">
-                     <h3 className="text-gray-200 text-xl font-semibold ">
-                        MAY 2023
+                  <div className="p-5 ">
+                     <h3 className="text-gray-200 md:text-[24px] text-[21px] font-semibold ">
+                        TBC
                      </h3>
-                     <h3 className="text-gray-50 text-2xl font-bold mt-1 mb-2">
-                        The CryptoBros
+                     <h3 className="text-gray-50 md:text-[34px] text-[28px] fontSfBold mt-1 mb-2">
+                        The Feds
                      </h3>
-                     <p className="text-gray-200 fontSfReg md:mr-5">
-                        The CryptoBros our primiere collection od 8,888 PFPs
-                        that serve as a membership card to utility and perks
-                        available exclusively to the Brotherhood.
+                     <p className="text-gray-200 fontSfReg md:mr-5 md:text-[20px]">
+                        The CryptoBros wouldn’t be complete without 888
+                        utility-enabled Feds who make the money printer go brrr.
+                        Rumour has it they’re coming for your $BRO.
                      </p>
-                     <button className="bg-white text-gray-950 rounded-lg px-3 py-1 mt-3">
-                        Learn More
-                     </button>
                   </div>
                </div>
                <div
-                  className=" rounded-md max- h-[260px]"
+                  className=" rounded-md max- lg:h-[300px] md:h-[330px] h-[300px]"
                   style={{
                      backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${img4})`,
                      backgroundRepeat: "no-repeat",
@@ -102,21 +99,18 @@ const OurCollection = () => {
                      width: "100%",
                   }}
                >
-                  <div className="p-5 lg:mt-4">
-                     <h3 className="text-gray-200 text-xl font-semibold ">
-                        MAY 2023
+                  <div className="p-5 ">
+                     <h3 className="text-gray-200 md:text-[24px] text-[21px] font-semibold ">
+                        TBC
                      </h3>
-                     <h3 className="text-gray-50 text-2xl font-bold mt-1 mb-2">
-                        The CryptoBros
+                     <h3 className="text-gray-50 md:text-[34px] text-[28px] fontSfBold mt-1 mb-2">
+                        II: Attention Economy
                      </h3>
-                     <p className="text-gray-200 fontSfReg md:mr-5">
-                        The CryptoBros our primiere collection od 8,888 PFPs
-                        that serve as a membership card to utility and perks
-                        available exclusively to the Brotherhood.
+                     <p className="text-gray-200 fontSfReg md:mr-5 md:text-[20px]">
+                        More ordinals, followers, views, and attention.
+                        CryptoBros go viral, become a hedge against inflation
+                        and trigger an NFT Bull-Run.
                      </p>
-                     <button className="bg-white text-gray-950 rounded-lg px-3 py-1 mt-3">
-                        Learn More
-                     </button>
                   </div>
                </div>
             </div>
