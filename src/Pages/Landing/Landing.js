7a1ba7f03a43ffbebeb0891b9587330e63c3b5db
unknown <ojitdebnathfreelancer.com>
2023-04-06 16:00:28 +0600

    side menu and main mane modify

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
    return (
-      <div className="max-container lg:px-0 md:px-4 px-4">
-         <div className="flex justify-between items-center mt-[16px]">
+      <div>
+         <div className="flex justify-between items-center mt-[16px] px-5">
             <div className="xl:h-[70px] lg:h-[60px] md:h-[50px] h-[40px]">
                <Link>
                   <img
@@ -30,17 +30,18 @@ const Landing = () => {
                </Link>
             </div>
          </div>
-
-         <div>
-            <div className="flex justify-center items-center xl:h-[500px] lg:h-[420px] md:h-[300px] h-[250px]">
-               <img className="h-full" src={landingImg} alt="landingImg" />
-            </div>
-            <div className="text-center">
-               <Link to="/home">
-                  <button className="border-2 border-gray-900 rounded xl:text-normal lg:text-[18px] md:text-[16px] text-[14px] xl:px-5 lg:px-4 md:px-2 px-2 py-2 font-trash duration-300 hover:bg-black hover:text-white">
-                     Enter The BroTheRhood
-                  </button>
-               </Link>
+         <div className="max-container lg:px-0 md:px-4">
+            <div>
+               <div className="flex justify-center items-center xl:h-[500px] lg:h-[420px] md:h-[300px] h-[250px]">
+                  <img className="h-full" src={landingImg} alt="landingImg" />
+               </div>
+               <div className="text-center">
+                  <Link to="/home">
+                     <button className="border-2 border-gray-900 rounded xl:text-normal lg:text-[18px] md:text-[16px] text-[14px] xl:px-5 lg:px-4 md:px-2 px-2 py-2 font-trash duration-300 hover:bg-black hover:text-white">
+                        Enter The BroTheRhood
+                     </button>
+                  </Link>
+               </div>
             </div>
          </div>
       </div>
